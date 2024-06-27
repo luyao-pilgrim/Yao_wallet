51ab8f3e030bec2a3d90c384fe68d3ce36437345
LuYao-pilgrim <luyao_shoottheball@163.com>
2024-06-27 13:26:26 +0800

    feat: wallet init

---
+++ src/pages/_app.tsx
@@ -0,0 +1,30 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, base } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const config = getDefaultConfig({
+  appName: "My RainbowKit App",
+  projectId: "b7eb0678d8a1177c797e5543c3162fb9",
+  chains: [mainnet, polygon, optimism, arbitrum, base],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+const queryClient = new QueryClient();
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
+
+export default App;
